835883bdcc8a8119155d36909dfceec34f3f38c0
Jørund Martinsen <joerundmartinsen@hotmail.com>
2018-10-04 11:04:22 +0200

    Added a project for testing purposes. Simple console app

---
+++ WebApp/MongoDB Connection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace MongoDB_Connection
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+
+        //This generates a new salt for salting passwords
+        private static readonly RNGCryptoServiceProvider random = new RNGCryptoServiceProvider();
+        private static int saltLengthLimit = 32;
+
+        private static byte[] GetSalt()
+        {
+            return GetSalt(saltLengthLimit);
+        }
+        private static byte[] GetSalt(int maximumSaltLength)
+        {
+            var salt = new byte[maximumSaltLength];
+            using (random)
+            {
+                random.GetNonZeroBytes(salt);
+            }
+
+            return salt;
+        }
+    }
+}
