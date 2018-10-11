219d27474e0daf89fb0a6c29485c6f38e91e9e08
reneartois <mrterje@hotmail.com>
2018-10-11 21:40:12 +0200

    Added search field, with communication view-controller-model

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers {
     public class HomeController : Controller {
@@ -43,12 +44,56 @@ namespace WebApp.Controllers {
             return View();
         }
 
-        public ActionResult Notimplemented() {
+        public ActionResult Notimplemented()
+        {
             ViewBag.Message = "Page for functions that have not been implemented.";
 
             return View();
         }
 
+        public ActionResult ValidateInput(Document doc)
+        {
+            //If search - pass input through
+            //If adding data, validate input
+
+            if (doc.Id.ToString() == "")
+            {
+                return RedirectToAction("Notimplemented");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
+        }
+
+        public ActionResult GetResults()
+        {
+            List<Document> lDoc = new List<Document>();
+            lDoc.Add(new Document());
+            lDoc.Add(new Document());
+
+            lDoc[0].Id = 5;
+            lDoc[0].Title = "Paper1";
+            lDoc[0].Firstname = "Albert";
+            lDoc[0].Lastname = "Einste";
+            lDoc[0].Keywords = "Light";
+            lDoc[0].Publisher = "Einstein Publishing Company";
+            lDoc[0].DatePublished = "05.05.20";
+
+            lDoc[1].Id = 5;
+            lDoc[1].Title = "Paper2";
+            lDoc[1].Firstname = "Marie";
+            lDoc[1].Lastname = "Curie";
+            lDoc[1].Keywords = "Uranium";
+            lDoc[1].Publisher = "Curie Publishing";
+            lDoc[1].DatePublished = "25.05.05";
+
+            ViewBag.data = lDoc;
+
+            return View();
+        }
+        
 
     }
 }
---
+++ WebApp/WebApp/Models/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WebApp.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Keywords { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string DatePublished { get; set; }
+    }
+}
