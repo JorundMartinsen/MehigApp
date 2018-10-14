7c128aca767fbf40087b446372291d03a26218b2
Jørund Martinsen <jorund@automasjonservice.no>
2018-10-14 18:18:20 +0200

    Added model Reports

---
+++ WebApp/WebApp/Models/Reports.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApp.Models.Documents;
+
+namespace WebApp.Models {
+    public class Reports {
+
+        public List<ReportDocument> ReportDocuments { get; set; }
+
+        public Reports() {
+
+        }
+    }
+}
