e74d59fd9c441f1826652bb1d300e69ae4efbcd2
Jørund Martinsen <jorund@automasjonservice.no>
2018-10-30 22:28:50 +0100

    Looked into dynamically creating classes from MongoDB. Its possible

--- WebApp/MongoDB Connection/Program.cs
+++ WebApp/MongoDB Connection/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
-using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MongoDB_Connection
 {
@@ -8,30 +13,84 @@ namespace MongoDB_Connection
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            MyDynamicClass myDynamicClass = new MyDynamicClass("SomeClass");
+            var myClass = myDynamicClass.CreateObject(new string[3] { "ID", "Name", "Joe" }, new Type[3] { typeof(int), typeof(string), typeof(DateTime)});
+            Type type = myClass.GetType();
 
-
-
-
-
-        //This generates a new salt for salting passwords
-        private static readonly RNGCryptoServiceProvider random = new RNGCryptoServiceProvider();
-        private static int saltLengthLimit = 32;
-
-        private static byte[] GetSalt()
-        {
-            return GetSalt(saltLengthLimit);
-        }
-        private static byte[] GetSalt(int maximumSaltLength)
-        {
-            var salt = new byte[maximumSaltLength];
-            using (random)
-            {
-                random.GetNonZeroBytes(salt);
+            foreach (PropertyInfo property in type.GetProperties()) {
+                Console.WriteLine(property.Name);
             }
+        }
 
-            return salt;
+    }
+
+    class MyDynamicClass {
+        AssemblyName assemblyName;
+        public MyDynamicClass(string ClassName) {
+            assemblyName = new AssemblyName(ClassName);
+        }
+        public object CreateObject(string[] PropertyNames, Type[] Types) {
+            if (PropertyNames.Length != Types.Length) {
+                Console.WriteLine("All properties should have a type");
+            }
+            TypeBuilder DynamicClass = CreateClass();
+            CreateConstructor(DynamicClass);
+            for (int i = 0; i < PropertyNames.Length; i++) {
+                CreateProperty(DynamicClass, PropertyNames[i], Types[i]);
+            }
+            Type type = DynamicClass.CreateType();
+            return Activator.CreateInstance(type);
+        }
+
+        private TypeBuilder CreateClass() {
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType(assemblyName.FullName,
+                                                                TypeAttributes.Public |
+                                                                TypeAttributes.Class |
+                                                                TypeAttributes.AutoClass |
+                                                                TypeAttributes.AnsiClass |
+                                                                TypeAttributes.BeforeFieldInit |
+                                                                TypeAttributes.AutoLayout, null);
+            return typeBuilder;
+        }
+
+        private void CreateConstructor(TypeBuilder typeBuilder) {
+            typeBuilder.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+        }
+
+        private void CreateProperty(TypeBuilder typeBuilder, string propertyName, Type propertyType) {
+            FieldBuilder fieldBuilder = typeBuilder.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+            PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+            MethodBuilder getPropMthdBldr = typeBuilder.DefineMethod("get_" + propertyName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, propertyType, Type.EmptyTypes);
+            ILGenerator getIl = getPropMthdBldr.GetILGenerator();
+
+            getIl.Emit(OpCodes.Ldarg_0);
+            getIl.Emit(OpCodes.Ldfld, fieldBuilder);
+            getIl.Emit(OpCodes.Ret);
+
+            MethodBuilder setPropMthdBldr = typeBuilder.DefineMethod("set_" + propertyName,
+                  MethodAttributes.Public |
+                  MethodAttributes.SpecialName |
+                  MethodAttributes.HideBySig,
+                  null, new[] { propertyType });
+
+            ILGenerator setIl = setPropMthdBldr.GetILGenerator();
+            Label modifyProperty = setIl.DefineLabel();
+            Label exitSet = setIl.DefineLabel();
+
+            setIl.MarkLabel(modifyProperty);
+            setIl.Emit(OpCodes.Ldarg_0);
+            setIl.Emit(OpCodes.Ldarg_1);
+            setIl.Emit(OpCodes.Stfld, fieldBuilder);
+
+            setIl.Emit(OpCodes.Nop);
+            setIl.MarkLabel(exitSet);
+            setIl.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getPropMthdBldr);
+            propertyBuilder.SetSetMethod(setPropMthdBldr);
         }
     }
 }
